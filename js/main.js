a0b0101efe7ca6ee0b796decc26940ae4c6c9d17
Kreig Durham <kreig.e.durham@gmail.com>
2017-09-16 15:32:40 -0700

    Messing Around
    Still doesn't work like I want, but still making attempts.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,32 @@
-window.onload = function(){
-        freesound.setToken("FuiD28l1Fo8cd4FeYJZSkPHo5A0y8JMM8yVfckox");
-        
-        var fields = 'id,name,url';
-        // Example 2
-        // Example of searching sounds: querying the freesound db for sounds
-        var query = "jazz";
-        var page = 1;
-//         var filter = "license:[Creative Commons 0]";
-        var filter = "tag:tenuto duration:[1.0 TO 15.0]"
-        var sort = "rating_desc";
-        freesound.textSearch(query, {page:page, filter:filter, sort:sort, fields:fields},
-            function(sounds){
-                var msg = ""
-                
-                msg = "<h3>Searching for: " + query + "</h3>"
-                msg += "With filter: " + filter +" and sorting: " + sort + "<br>"
-                msg += "Num results: " + sounds.count + "<br><ul>"
-                for (i =0;i<=10;i++){  
-                    var snd = sounds.getSound(i)
-                    msg += "<li>" + snd.name + " by " + snd.username + "</li>"
-                }
-                msg += "</ul>"
-                msg += "<img src='" + sound.images.waveform_l + "'>";
-                snd = new Audio(sound.previews['preview-hq-mp3']);
-                msg += '<br><button onclick="snd.play()">play</button><button onclick="snd.pause()">pause</button><br><br>';
-                displayMessage(msg,"resp1");
-            },function(){ displayError("Error while searching...")}
-        );
+ window.onload = function () {
+   freesound.setToken("FuiD28l1Fo8cd4FeYJZSkPHo5A0y8JMM8yVfckox");
 
-    function displayError(text){
-        document.getElementById('error').innerHTML=text;
-    }
-    function displayMessage(text,place){
-        document.getElementById(place).innerHTML=text;
-    }
-};
+   var fields = 'id,name,url';
+   // Example of geeting the info of a sound, queying for similar sounds (content based) and showing some analysis
+   // features. Both similar sounds and analysis features are obtained with additional requests to the api.
+   var query = "piano"
+   freesound.textSearch(query,
+     function (sound) {
+       var msg = "";
+       msg = "<h3>Getting info of sound: " + sound.name + "</h3>";
+       msg += "<strong>Url:</strong> " + sound.url + "<br>";
+       msg += "<strong>Description:</strong> " + sound.description + "<br>";
+       msg += "<img src='" + sound.images.waveform_l + "'>";
+       snd = new Audio(sound.previews['preview-hq-mp3']);
+       msg += '<br><button onclick="snd.play()">play</button><button onclick="snd.pause()">pause</button><br><br>';
+       displayMessage(msg, 'resp1');
+       console.log(XMLHttpRequest.status);
+     },
+     function () {
+       displayError("Sound could not be retrieved.")
+     }
+   );
+
+   function displayError(text) {
+     document.getElementById('resp1').innerHTML = text;
+   }
+
+   function displayMessage(text, place) {
+     document.getElementById(place).innerHTML = text;
+   }
+ }
